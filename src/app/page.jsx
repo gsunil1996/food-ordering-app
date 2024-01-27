f3b20379a5eab5c45145e98da002ae039264e4eb
gsunil1996 <gajulasunilkumar123@gmail.com>
2024-01-27 12:24:54 +0530

    update

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header"
+import Hero from "@/components/layout/Hero"
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+export default Home
